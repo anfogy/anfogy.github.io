d91cff02c8de02d2553bd07d125ae3274277e7e5
anfogy <76080409+anfogy@users.noreply.github.com>
2022-12-21 22:53:50 +0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,44 @@
+var points = 0;
 
+var canvas = document.getElementById("ballCanv");
+var ctx = canvas.getContext("2d");
+var ballRadius = 20;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+        points++;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+        points++;
+    }
+    
+    x += dx;
+    y += dy;
+}
+
+function updatePoints() {
+   document.getElementById("points").innerText = points;
+}
+
+function tickUpdate() {
+   draw();
+   updatePoints();
+}
+setInterval(tickUpdate);

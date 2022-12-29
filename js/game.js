72e7701de2be3adb4726778e0d1e695cfc2f8daf
anfogy <76080409+anfogy@users.noreply.github.com>
2022-12-29 10:19:52 +0800

    Last time… Trust me….

--- js/game.js
+++ js/game.js
@@ -3,22 +3,31 @@ var points = 1;
 var canvas = document.getElementById("ballCanv");
 var ctx = canvas.getContext("2d");
 var ballRadius = 20;
-var x = canvas.width/2;
-var y = canvas.height-30;
+var x = Math.random() * (canvas.width - 60) + 10;
+var y = Math.random() * (canvas.height - 100);
 
-var dx = 0.3;
+var dx = 0.45;
+var dxd = 0;
 var dy = -dx;
+var dyd = 1;
 var speed = dx;
 
 function setSpeed(sp) {
- dx += sp;
- dy -= sp;
- speed = sp;
+  if (dxd % 2) {
+    dx = -sp;
+  } else {
+    dx = sp;
+  } if (dyd % 2) {
+    dy = -sp;
+  } else {
+    dy = sp;
+  }
+  speed = sp;
 }
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
@@ -28,31 +37,31 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
 
-  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-      dx = -dx;
-      edgeHitEvent();
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dxd++;
+    dx = -dx;
+    edgeHitEvent();
   }
-  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-      dy = -dy;
-      edgeHitEvent();   
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dyd++;
+    dy = -dy;
+    edgeHitEvent();
   }
-
   x += dx;
   y += dy;
 }
 
 function edgeHitEvent() {
   points++;
-  setSpeed(speed*1.05);
-  setSpeed(speed*(25-speed)/25);
+  setSpeed(speed * Math.max(points * 0.05, 1));
 }
 
 function updatePoints() {
-   document.getElementById("points").innerText = points;
+  document.getElementById("points").innerText = points;
 }
 
 function tickUpdate() {
-   draw();
-   updatePoints();
+  draw();
+  updatePoints();
 }
 setInterval(tickUpdate);

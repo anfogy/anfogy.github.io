cec8d8868ba4580a10dc626213185ae1b58493bd
anfogy <76080409+anfogy@users.noreply.github.com>
2022-12-31 19:57:50 +0800

    Add save system and news ticker

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 var version = 1;
 
-var points = 1;
+var points = 0;
 var orSpeed = 0.3
 
 var canvas = document.getElementById("ballCanv");
@@ -16,6 +16,8 @@ var dyd = 1;
 var speed = dx;
 
 var lastCalcTime = 0;
+
+load();
 
 function setSpeed(sp) {
   if (dxd % 2) {
@@ -38,10 +40,7 @@ function drawBall() {
   ctx.closePath();
 }
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall();
-
+function ballTrack() {
   if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
     dxd++;
     dx = -dx;
@@ -59,6 +58,7 @@ function draw() {
 function edgeHitEvent() {
   points++;
   setSpeed(orSpeed + (Math.log(points)**1.3));
+  updatePoints();
 }
 
 function updatePoints() {
@@ -66,7 +66,62 @@ function updatePoints() {
 }
 
 function tickUpdate() {
-  draw();
+  ballTrack();
+}
+
+function refreshUpdate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+}
+
+var tickInterval = setInterval(tickUpdate);
+var refreshInterval = setInterval(refreshUpdate);
+var saveInterval = setInterval(function(){
+  document.cookie = "save=" + points+";SameSite=Lax";
+}, 5000)
+
+function load() {
+  points = getCookie("save");
+  setSpeed(orSpeed + (Math.log(points)**1.3));
   updatePoints();
 }
-setInterval(tickUpdate);
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+//-------forget the balls, get into some news bar now-------//
+
+var news = document.getElementById('news');
+var guh = document.getElementById('guh');
+
+var w = 400;
+guh.style.width = w + 'px';
+var s = 420;
+
+function moveNews() {
+  news.style.left = s +'px';
+  if (s <= 0 - w + 1) {
+    s = w - 1;
+  }else {
+    s = s - 1;
+  }
+}
+
+function changeNews() {
+  const news = ["This is a very important news!", ":blob:", "Guh?"]
+}
+
+var newsTextInterval = setInterval(moveNews, 70);
